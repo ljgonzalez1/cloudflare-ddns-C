3297111ccf5b9e75fc080aec50aa21d441fda9b4
Luis González <ljgonzalez@uc.cl>
2025-06-01 17:41:54 -0400

    ✨ feat(`src/cloudflare_ddns/messages`): Implement message structure with emojis.

--- src/cloudflare_ddns/messages/messages.c
+++ src/cloudflare_ddns/messages/messages.c
@@ -1,21 +1,46 @@
 #include "messages.h"
 
-
-static const char *get_local_lang_env(void) {
-  const char *env = getenv("APP_LANG");
-  const char *lang_code = NULL;
-
-  if (env != NULL && strlen(env) == 2) {
-    lang_code = env;
+// TODO: complete locale implementation
+// TODO: English only for now
+// NOTE: As a buffer is used, message + emoji can't be larger than 254 characters.
+static const Messages all_messages[] = {
+  {
+    .msg_key    = "MSG_PROGRAM_START",
+    .msg_emoji  = "\uD83D\uDE80",
+    .msg_en_US = "Starting program...",
+  },
+  {
+    .msg_key    = "MSG_PROGRAM_END",
+    .msg_emoji  = "\uD83C\uDFC1",
+    .msg_en_US  = "Finished execution. Now closing.",
+  },
+  {
+    .msg_key    = "ERR_VAR_NOT_FOUND",
+    .msg_emoji  = "❌",
+    .msg_en_US  = "Environment variable not found.",
   }
-}
+};
 
-
-
+#define NUM_MESSAGES (sizeof(all_messages) / sizeof(all_messages[0]))
 
 const char *message(const char *msg_key) {
+  if (msg_key == NULL) {
+    return NULL;
+  }
 
+  static char buf[256];
+
+  for (size_t i = 0; i < NUM_MESSAGES; ++i) {
+    if (strcmp(all_messages[i].msg_key, msg_key) == 0) {
+
+      /* Chain together "<emoji> <text>" */
+      snprintf(buf, sizeof buf, "%s %s",
+               all_messages[i].msg_emoji,
+               all_messages[i].msg_en_US);
+
+      return buf;
+    }
+  }
+
+  return NULL;
 }
-
-
-
--- src/cloudflare_ddns/messages/messages.h
+++ src/cloudflare_ddns/messages/messages.h
@@ -1,5 +1,19 @@
 #pragma once
 
 #include <string.h>
+#include <stdio.h>
 
-const char *message_val(const char *msg_key);
+#define DEFAULT_LOCALE "en_US"
+
+struct messages {
+  const char *lang_code;
+  const char *msg_emoji;
+
+  const char *msg_en_US;
+  const char *msg_es_ES;
+  // TODO: add more translations as needed
+};
+
+typedef struct messages Messages;
+
+const char *message(const char *msg_key);
